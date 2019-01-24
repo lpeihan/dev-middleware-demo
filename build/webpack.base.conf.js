6066032bc464a6eb64b2b131c9b549b569fbe0c8
李培汉 <lipeihan@chandashi.com>
2019-01-24 17:33:19 +0800

    style: 新增样式

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -20,9 +20,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       vue$: 'vue/dist/vue.esm.js',
-      '@': resolve(dir.src),
-      'styles': resolve(`${dir.src}/assets/styles`),
-      'imgs': resolve(`${dir.src}/assets/imgs`)
+      '@': resolve(dir.src)
     }
   },
   module: {
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <img src="@/assets/imgs/logo.png">
     <router-link to="/login">login</router-link>
   </div>
 </template>
