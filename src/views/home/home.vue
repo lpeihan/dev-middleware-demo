fceed56364666951e4ab0def34a7d1e3219a32d1
李培汉 <lipeihan@chandashi.com>
2019-01-25 16:12:58 +0800

    perf: 获取接口数据

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,3 +4,13 @@
     <router-link to="/login">login</router-link>
   </div>
 </template>
+
+<script>
+import { getExample } from '@/api/example';
+
+export default {
+  mounted() {
+    getExample();
+  }
+};
+</script>
